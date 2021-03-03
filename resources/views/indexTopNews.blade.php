3e7ae3e1588e9c79927153cf8bfb0356f1071e04
Darkhan <31818912+Dark-Han@users.noreply.github.com>
2021-03-03 23:49:25 +0600

    исправление бага с отображением главных новостей

--- resources/views/indexTopNews.blade.php
+++ resources/views/indexTopNews.blade.php
@@ -1,3 +1,4 @@
+                            @if(isset($topNews[0]))
                             <div class="col-xl-8 col-sm-12 main-banner-img">
                                 <div class="big-img-news">
                                     <a href="{{$locale."/".$topNews[0]->categorySlug."/".$topNews[0]->slug}}">
@@ -9,31 +10,36 @@
                                     </a>
                                 </div>
                             </div>
+                            @endif
 
-                            <div class="col-xl-4 col-md-12 top-img">
-                            @for($i=1;$i<3;$i++)
-                                    <div class="small-img-news">
-                                        <a href="inner-news.php?id_cat=15 &amp;&amp; id=166297">
-                                            <img src="{{asset('storage/'.$topNews[$i]->cover)}}" alt="" class="img-top-news" style="width: 100% !important;height:100%">
-                                            <div class="big-news-caption">
-                                                <p>Общество <span><img src="images/view-icon-white.png" alt="">{{$topNews[$i]->seen}}</span></p>
-                                                <h5>{{$topNews[$i]->title}}</h5>
+                            @if(isset($topNews[1],$topNews[2]))
+                                <div class="col-xl-4 col-md-12 top-img">
+                                    @for($i=1;$i<=2;$i++)
+                                            <div class="small-img-news">
+                                                <a href="{{$locale."/".$topNews[$i]->categorySlug."/".$topNews[$i]->slug}}">
+                                                    <img src="{{asset('storage/'.$topNews[$i]->cover)}}" alt="" class="img-top-news" style="width: 100% !important;height:100%">
+                                                    <div class="big-news-caption">
+                                                        <p>Общество <span><img src="images/view-icon-white.png" alt="">{{$topNews[$i]->seen}}</span></p>
+                                                        <h5>{{$topNews[$i]->title}}</h5>
+                                                    </div>
+                                                </a>
                                             </div>
-                                        </a>
-                                    </div>
-                            @endfor
-                            </div>
-
-                            @for($i=3;$i<=5;$i++)
-                            <div class="col-xl-4 col-md-6">
-                                <div class="small-img-news">
-                                    <a href="inner-news.php?id_cat=73 &amp;&amp; id=166284">
-                                          <img src="{{asset('storage/'.$topNews[$i]->cover)}}" alt="" class="img-top-news" style="width: 100% !important;height:100%">
-                                          <div class="big-news-caption">
-                                          <p>Есть мнение! <span><img src="images/view-icon-white.png" alt="">{{$topNews[$i]->seen}}</span></p>
-                                          <h5>{{$topNews[$i]->title}}</h5>
-                                          </div>
-                                    </a>
+                                    @endfor
                                 </div>
-                            </div>
-                            @endfor
+                            @endif
+
+                            @if(isset($topNews[3]))
+                                @for($i=3;$i<$topNews->count();$i++)
+                                    <div class="col-xl-4 col-md-6">
+                                        <div class="small-img-news">
+                                           <a href="{{$locale."/".$topNews[$i]->categorySlug."/".$topNews[$i]->slug}}">
+                                                  <img src="{{asset('storage/'.$topNews[$i]->cover)}}" alt="" class="img-top-news" style="width: 100% !important;height:100%">
+                                                  <div class="big-news-caption">
+                                                  <p>Есть мнение! <span><img src="images/view-icon-white.png" alt="">{{$topNews[$i]->seen}}</span></p>
+                                                  <h5>{{$topNews[$i]->title}}</h5>
+                                                  </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endfor
+                            @endif
